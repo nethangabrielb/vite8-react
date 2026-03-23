7b4f509ff9a402f65e2b415d03a87899e6197f5b
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-03-24 00:16:27 +0800

    chorer: remove unused UI component exports

--- src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Input } from "./Input";
-export { Button } from "./MotionButton";
-export { Reveal } from "./Reveal";
-export { Textarea } from "./Textarea";
-export { ThemeToggle } from "./ThemeToggle";
